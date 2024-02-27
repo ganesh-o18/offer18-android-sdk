bb9e004b569ffe670e4e3e6bb935bba38058a905
ganesh-o18 <142499462+ganesh-o18@users.noreply.github.com>
2024-02-27 12:33:58 +0530

    Refactor Constant

--- offer18-android-sdk/src/main/java/com/offer18/sdk/constant/Constant.java
+++ offer18-android-sdk/src/main/java/com/offer18/sdk/constant/Constant.java
@@ -4,4 +4,12 @@ public class Constant {
     public static final String ENV_DEBUG = "debug";
     public static final String ENV_PRODUCTION = "production";
     public static final String ENDPOINT_TRACK_IMPRESSION = "https://api.offer18.com/track-impression";
+    public static final String DOMAIN = "domain";
+    public static final String ACCOUNT_ID = "accountId";
+    public static final String ADV_SUB_1 = "advSub1";
+    public static final String ADV_SUB_2 = "advSub2";
+    public static final String ADV_SUB_3 = "advSub3";
+    public static final String ADV_SUB_4 = "advSub4";
+    public static final String ADV_SUB_5 = "advSub5";
+    public static final String OFFER_ID = "offerId";
 }
--- offer18-android-sdk/src/test/java/com/offer18/sdk/Offer18ClientUnitTest.java
+++ offer18-android-sdk/src/test/java/com/offer18/sdk/Offer18ClientUnitTest.java
@@ -3,6 +3,8 @@ package com.offer18.sdk;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import com.offer18.sdk.constant.Constant;
+
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -15,19 +17,27 @@ public class Offer18ClientUnitTest {
     @Test
     public void sdk_init_does_not_throws_exceptions() throws Exception {
         Offer18.init(new HashMap<>());
+    }
+
+    @Test
+    public void client_endpoint_returns_valid_url() throws Exception {
+        Offer18.init(new HashMap<>());
         Map<String, String> args = new HashMap<>();
         String domain = "ganesh-local-dev.o18-test.live";
         String accountId = "18962";
         String offerId = "20520286";
-        args.put("domain", domain);
-        args.put("accountId", accountId);
-        args.put("offerId", offerId);
+        String advSub1 = "1";
+        args.put(Constant.DOMAIN, domain);
+        args.put(Constant.ACCOUNT_ID, accountId);
+        args.put(Constant.OFFER_ID, offerId);
+        args.put(Constant.ADV_SUB_1, advSub1);
         HttpUrl url = new Offer18Client().buildEndpoint(args);
         assertNotNull(url.toString());
         System.out.println(url.url());
         assertEquals(url.url().getHost(), domain);
         assertEquals(url.queryParameter("m"), accountId);
         assertEquals(url.queryParameter("o"), offerId);
+        assertEquals(url.queryParameter("adv_sub1"), advSub1);
     }
 }
 
